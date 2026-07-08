094ba2176964ac18de0d1cadd93b59109540d905
Lucas <lcorte110210@gmail.com>
2026-07-08 19:23:36 -0300

    Estruturando página de anotações

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, Alert } from 'react-native'
 
 type NoteProps = {
   children: React.ReactNode;
@@ -7,10 +7,34 @@ type NoteProps = {
 }
 
 export default function Note({children, subtitle, hidden}: NoteProps) {
+  function openModal() {
+    Alert.alert(
+      "Opções",
+      "O que deseja fazer?",
+      [
+        {
+          text: "Renomear",
+          onPress: () => console.log("Renomear"),
+        },
+        {
+          text: "Excluir",
+          style: "destructive",
+          onPress: () => console.log("Excluir"),
+        },
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+      ]
+    );
+  }
+
   return (
     <TouchableOpacity
       className={`w-full gap-1 border-2 border-[#DCDCDC] rounded-xl p-4 mb-5 ${hidden ? "hidden" : ""}`}
       activeOpacity={.75}
+      delayLongPress={1000}
+      onLongPress={openModal}
     >
       <Text className="font-bold">{children}</Text>
       <Text className="text-[#888] italic truncate">{subtitle}</Text>
